09d1a0b888d4a2f6bd17fb2909245c5a272ced5c
Al <albarrentine@gmail.com>
2025-07-28 02:01:34 -0400

    [test] refactoring test code for new xoshiro PRNGs

--- test.c
+++ test.c
@@ -1,86 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <float.h>
 
 #include "greatest/greatest.h"
 
-#include "rand32.h"
-#include "rand64.h"
+#include "rand_u32.h"
+#include "rand_u64.h"
+#include "rand_float.h"
+#include "rand_double.h"
 
 TEST rand32_test(void) {
-    uint32_t random_value = rand32();
+    rand_u32_t rng;
+    rand_u32_init(&rng);
+    uint32_t random_value = rand_u32(&rng);
     PASS();
 }
 
 TEST rand32_seed_test(void) {
-    rand32_seed(123456, 987654);
+    rand_u32_t rng;
+    rand_u32_init_seed(&rng, 123456);
     PASS();
 }
 
 TEST rand32_bounded_test(void) {
-    uint32_t random_value = rand32_bounded(100);
+    rand_u32_t rng;
+    rand_u32_init(&rng);
+    uint32_t random_value = rand_u32_bounded(&rng, 100);
     ASSERT_LT(random_value, 100);
-    PASS();
-}
-
-TEST rand32_gen_test(void) {
-    rand32_gen_t rng = rand32_gen_init();
-    rand32_gen_seed_os(&rng);
-    uint32_t random_value = rand32_gen_random(&rng);
-    PASS();
-}
-
-TEST rand32_gen_bounded_test(void) {
-    rand32_gen_t rng = rand32_gen_init();
-    rand32_gen_seed(&rng, 123456789, 987654321);
-    uint32_t random_value = rand32_gen_bounded(&rng, 100);
-    ASSERT_LT(random_value, 100);
-    PASS();
-}
-
-TEST rand32_gen_advance_test(void) {
-    rand32_gen_t rng = rand32_gen_init();
-    rand32_gen_seed(&rng, 123456789, 987654321);
-    rand32_gen_advance(&rng, 100);
-    uint32_t random_value = rand32_gen_random(&rng);
     PASS();
 }
 
 TEST rand64_test(void) {
-    uint64_t random_value = rand64();
+    rand_u64_t rng;
+    rand_u64_init(&rng);
+    uint64_t random_value = rand_u64(&rng);
     PASS();
 }
 
 TEST rand64_seed_test(void) {
-    rand64_seed(1234567890123456789ULL, 9876543210987654321ULL);
+    rand_u64_t rng;
+    rand_u64_init_seed(&rng, 1234567890123456789ULL);
     PASS();
 }
 
 TEST rand64_bounded_test(void) {
-    uint64_t random_value = rand64_bounded(1234567890123456789ULL);
+    rand_u64_t rng;
+    rand_u64_init(&rng);
+    uint64_t random_value = rand_u64_bounded(&rng, 1234567890123456789ULL);
     ASSERT_LT(random_value, 1234567890123456789ULL);
     PASS();
 }
 
-TEST rand64_gen_test(void) {
-    rand64_gen_t rng = rand64_gen_init();
-    rand64_gen_seed_os(&rng);
-    uint64_t random_value = rand64_gen_random(&rng);
+TEST rand_float_uniform_test(void) {
+    rand_float_t rng;
+    rand_float_init(&rng);
+    float random_value = rand_float_uniform(&rng);
+    ASSERT_LT(random_value, 1.0f + FLT_EPSILON);
+    ASSERT_GT(random_value, 0.0f - FLT_EPSILON);
     PASS();
 }
 
-TEST rand64_gen_bounded_test(void) {
-    rand64_gen_t rng = rand64_gen_init();
-    rand64_gen_seed(&rng, 123456789, 987654321);
-    uint64_t random_value = rand64_gen_bounded(&rng, 100);
-    ASSERT_LT(random_value, 100);
+TEST rand_float_bounded_test(void) {
+    rand_float_t rng;
+    rand_float_init(&rng);
+    float random_value = rand_float_bounded(&rng, 0.0f, 100.0f);
+    ASSERT_LT(random_value, 100.0f + FLT_EPSILON);
+    ASSERT_GT(random_value, 0.0f - FLT_EPSILON);
     PASS();
 }
 
-TEST rand64_gen_advance_test(void) {
-    rand64_gen_t rng = rand64_gen_init();
-    rand64_gen_seed(&rng, 123456789, 987654321);
-    rand64_gen_advance(&rng, 100);
-    uint64_t random_value = rand64_gen_random(&rng);
+TEST rand_float_test(void) {
+    rand_float_t rng;
+    rand_float_init(&rng);
+    float random_value = rand_float(&rng);
+    PASS();
+}
+
+TEST rand_double_uniform_test(void) {
+    rand_double_t rng;
+    rand_double_init(&rng);
+    double random_value = rand_double_uniform(&rng);
+    ASSERT_LT(random_value, 1.0 + DBL_EPSILON);
+    ASSERT_GT(random_value, 0.0 - DBL_EPSILON);
+    PASS();
+}
+
+TEST rand_double_bounded_test(void) {
+    rand_double_t rng;
+    rand_double_init(&rng);
+    double random_value = rand_double_bounded(&rng, 0.0, 100.0);
+    ASSERT_LT(random_value, 100.0 + DBL_EPSILON);
+    ASSERT_GT(random_value, 0.0 - DBL_EPSILON);
+    PASS();
+}
+
+TEST rand_double_test(void) {
+    rand_double_t rng;
+    rand_double_init(&rng);
+    double random_value = rand_double(&rng);
     PASS();
 }
 
@@ -89,15 +107,15 @@ SUITE(random_tests) {
     RUN_TEST(rand32_test);
     RUN_TEST(rand32_bounded_test);
     RUN_TEST(rand32_seed_test);
-    RUN_TEST(rand32_gen_test);
-    RUN_TEST(rand32_gen_bounded_test);
-    RUN_TEST(rand32_gen_advance_test);
     RUN_TEST(rand64_test);
     RUN_TEST(rand64_seed_test);
     RUN_TEST(rand64_bounded_test);
-    RUN_TEST(rand64_gen_test);
-    RUN_TEST(rand64_gen_bounded_test);
-    RUN_TEST(rand64_gen_advance_test);
+    RUN_TEST(rand_float_test);
+    RUN_TEST(rand_float_uniform_test);
+    RUN_TEST(rand_float_bounded_test);
+    RUN_TEST(rand_double_test);
+    RUN_TEST(rand_double_uniform_test);
+    RUN_TEST(rand_double_bounded_test);
 }
 
 GREATEST_MAIN_DEFS();
