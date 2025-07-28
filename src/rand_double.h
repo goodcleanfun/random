ca8f3829effc0472c8c0bc990db74bfb194b179b
Al <albarrentine@gmail.com>
2025-07-28 01:47:49 -0400

    [random/double] using xoshiro256+, the fastest generator for floating point numbers, which benchmarks a little bit faster than xoshiro256+, with the caveat that the lower three bits may fail linearity tests, though that's fine since only the upper 53 are used for doubles

---
+++ src/rand_double.h
@@ -0,0 +1,153 @@
+/*  Written in 2018 by David Blackman and Sebastiano Vigna (vigna@acm.org)
+
+To the extent possible under law, the author has dedicated all copyright
+and related and neighboring rights to this software to the public domain
+worldwide.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+
+#ifndef RAND_DOUBLE_H
+#define RAND_DOUBLE_H
+
+#include <stdint.h>
+
+/* This is xoshiro256+ 1.0, our best and fastest generator for floating-point
+   numbers. We suggest to use its upper bits for floating-point
+   generation, as it is slightly faster than xoshiro256++/xoshiro256**. It
+   passes all tests we are aware of except for the lowest three bits,
+   which might fail linearity tests (and just those), so if low linear
+   complexity is not considered an issue (as it is usually the case) it
+   can be used to generate 64-bit outputs, too.
+
+   We suggest to use a sign test to extract a random Boolean value, and
+   right shifts to extract subsets of bits.
+
+   The state must be seeded so that it is not everywhere zero. If you have
+   a 64-bit seed, we suggest to seed a splitmix64 generator and use its
+   output to fill s. */
+
+#include "rand_os.h"
+#include "rand_seed.h"
+#include "rotl.h"
+
+#define RAND_DOUBLE_STATE_SIZE 4
+
+typedef struct {
+    uint64_t state[RAND_DOUBLE_STATE_SIZE];
+} rand_double_t;
+
+static inline void rand_double_init_seed(rand_double_t *rng, uint64_t seed) {
+    rand_state_init(seed, rng->state, RAND_DOUBLE_STATE_SIZE);
+}
+
+static inline void rand_double_init(rand_double_t *rng) {
+    rand_double_init_seed(rng, os_random_seed());
+}
+
+static inline uint64_t rand_double_next_raw(rand_double_t *rng) {
+    uint64_t *s = rng->state;
+
+	const uint64_t result = s[0] + s[3];
+
+	const uint64_t t = s[1] << 17;
+
+	s[2] ^= s[0];
+	s[3] ^= s[1];
+	s[1] ^= s[2];
+	s[0] ^= s[3];
+
+	s[2] ^= t;
+
+	s[3] = rotl64(s[3], 45);
+
+	return result;
+}
+
+static inline double bits_to_double(uint64_t bits) {
+    return (bits >> 11) * 0x1.0p-53;
+}
+
+static inline double rand_double_next(rand_double_t *rng) {
+	uint64_t bits = rand_double_next_raw(rng);
+    return bits_to_double(bits);
+}
+
+static inline double rand_double_uniform(rand_double_t *rng) {
+    uint64_t bits = rand_double_next_raw(rng);
+	return (bits >> 11) * (1.0 / (1ULL << 53));
+}
+
+static inline double rand_double_bounded(rand_double_t *rng, double low, double high) {
+    return low + (high - low) * rand_double_uniform(rng);
+}
+
+/* This is the jump function for the generator. It is equivalent
+   to 2^128 calls to next(); it can be used to generate 2^128
+   non-overlapping subsequences for parallel computations. */
+
+static inline void rand_double_jump(rand_double_t *rng) {
+	uint64_t *s = rng->state;
+	static const uint64_t JUMP[] = { 0x180ec6d33cfd0aba, 0xd5a61266f0c9392c, 0xa9582618e03fc9aa, 0x39abdc4529b1661c };
+
+	uint64_t s0 = 0;
+	uint64_t s1 = 0;
+	uint64_t s2 = 0;
+	uint64_t s3 = 0;
+	for(int i = 0; i < sizeof JUMP / sizeof *JUMP; i++)
+		for(int b = 0; b < 64; b++) {
+			if (JUMP[i] & UINT64_C(1) << b) {
+				s0 ^= s[0];
+				s1 ^= s[1];
+				s2 ^= s[2];
+				s3 ^= s[3];
+			}
+			rand_double_next(rng);
+		}
+		
+	s[0] = s0;
+	s[1] = s1;
+	s[2] = s2;
+	s[3] = s3;
+}
+
+
+/* This is the long-jump function for the generator. It is equivalent to
+   2^192 calls to next(); it can be used to generate 2^64 starting points,
+   from each of which jump() will generate 2^64 non-overlapping
+   subsequences for parallel distributed computations. */
+
+static inline void rand_double_long_jump(rand_double_t *rng) {
+	uint64_t *s = rng->state;
+	static const uint64_t LONG_JUMP[] = { 0x76e15d3efefdcbbf, 0xc5004e441c522fb3, 0x77710069854ee241, 0x39109bb02acbe635 };
+
+	uint64_t s0 = 0;
+	uint64_t s1 = 0;
+	uint64_t s2 = 0;
+	uint64_t s3 = 0;
+	for(int i = 0; i < sizeof LONG_JUMP / sizeof *LONG_JUMP; i++)
+		for(int b = 0; b < 64; b++) {
+			if (LONG_JUMP[i] & UINT64_C(1) << b) {
+				s0 ^= s[0];
+				s1 ^= s[1];
+				s2 ^= s[2];
+				s3 ^= s[3];
+			}
+			rand_double_next(rng);
+		}
+		
+	s[0] = s0;
+	s[1] = s1;
+	s[2] = s2;
+	s[3] = s3;
+}
+
+#endif
